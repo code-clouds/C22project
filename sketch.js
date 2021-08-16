5efc25b02ce57e30f8e76874230533306f518d42
cluod <86750635+code-clouds@users.noreply.github.com>
2021-08-16 12:36:22 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,53 +15,71 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	var roof_options={
-		isStatic:true			
+	var roof_options = {
+		isStatic: true
 	}
 
-	roof = Bodies.rectangle(400,100,230,20,roof_options);
-    World.add(world,roof);
+	roof = Bodies.rectangle(400, 100, 230, 20, roof_options);
+	World.add(world, roof);
+	var options = {
+		isStatic : false,
+		restitution: 1,
+		friction:0,
+		density: 0.8
+	}
+	bob1 = Bodies.circle(320, 380, 20, options);
+	World.add(world, bob1);
 
-	con = Matter.Constraint.create({
-		pointA:{x:200,y:20},
-		bodyB:bobs,
-		pointB:{x:0,y:0},
-		length:100,
-		stiffness:0.1
-	  });
+	bob2 = Bodies.circle(360, 380, 20, options);
+	World.add(world, bob2);
 
-	World.add(world,con);	
+	bob3 = Bodies.circle(400, 380, 20, options);
+	World.add(world, bob3);
 
-rope1 = new rope(bobs,roof,300,350);
-rope2 = new rope(bobs,roof,350,350);
-rope3 = new rope(bobs,roof,400,350);
-rope4 = new rope(bobs,roof,450,350);
-rope5 = new rope(bobs,roof,500,350);
+	bob4 = Bodies.circle(440, 380, 20, options);
+	World.add(world, bob4);
+
+	bob5 = Bodies.circle(480, 380, 20, options);
+	World.add(world, bob5);
+
+	rope1 = new rope(bob1, roof, -80, 0);
+	rope2 = new rope(bob2, roof, -40, 0);
+	rope3 = new rope(bob3, roof, 0, 0);
+	rope4 = new rope(bob4, roof, 40, 0);
+	rope5 = new rope(bob5, roof, 80, 0);
 
 
 	Engine.run(engine);
-	
-  
+
+
 }
 
 function draw() {
-  rectMode(CENTER);
-  background("#99004d");
+	rectMode(CENTER);
+	background("#99004d");
 
-  rect(roof.position.x,roof.position.y,230,20);
+	rect(roof.position.x, roof.position.y, 230, 20);
 
-  //call display() to show ropes here
+	//call display() to show ropes here
+	rope1.display();
+	rope2.display();
+	rope3.display();
+	rope4.display();
+	rope5.display();
 
+	//create ellipse shape for multiple bobs here
+	ellipse(bob1.position.x, bob1.position.y, 40,40);
+	ellipse(bob2.position.x, bob2.position.y, 40,40);
+	ellipse(bob3.position.x, bob3.position.y, 40,40);
+	ellipse(bob4.position.x, bob4.position.y, 40,40);
+	ellipse(bob5.position.x, bob5.position.y, 40,40);
 
-
-  
-  //create ellipse shape for multiple bobs here
-	ellipse(300,350,50);
-	ellipse(350,350,50);
-	ellipse(400,350,50);
-	ellipse(450,350,50);
-	ellipse(500,350,50);
- 
 }
 
 //Write keyPressed function and apply force on pressing up_arrow key on the first bob.
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(bob1,bob1.position,{x:-50,y:-45});
+	}
+}
